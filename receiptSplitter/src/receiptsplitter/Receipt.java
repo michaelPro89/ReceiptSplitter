f9bf9c478a9fffb69bb5becb0fdd9b1af558e2c2
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 14:40:12 +0100

    Update Receipt.java

--- receiptSplitter/src/receiptsplitter/Receipt.java
+++ receiptSplitter/src/receiptsplitter/Receipt.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package receiptsplitter;
-
-/**
- *
- * @author Michal
- */
+/* I am working on an application Receipt Splitter which will allow user to choose currency from the list and split all expenses equally between everyone 
+* or split them by fixed amount. The application is in progress and will be finished soon.
+*
+* @author Michal Switala
+* Copyright © 2021 belongs solely to Michal Switala. 
+* You can reach me out on : https://github.com/michaelPro89
+*/
 public class Receipt {
     
     
